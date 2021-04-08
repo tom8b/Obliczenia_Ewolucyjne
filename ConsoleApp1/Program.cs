ff10059a360646cea46f8911641ac28e898743f8
kgogolaa <gogolinex@interia.pl>
2021-04-08 21:00:02 +0200

    ultimate algorithm
    Wspaniale zrobiony algorytm

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,12 +7,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var individualGenerator = new IndividualGenerator();
-            var generatedList = individualGenerator.GenerateList(3, 6, -10, 10);
-            var x = generatedList.First().X1Binary;
-            var xx = generatedList.First().GetX1Dec();
+            //var individualGenerator = new IndividualGenerator();
+            //var generatedList = individualGenerator.GenerateList(3, 6, -10, 10);
+            //var x = generatedList.First().X1Binary;
+            //var xx = generatedList.First().GetX1Dec();
 
-            Console.WriteLine("Hello World!");
         }
     }
 }
