3df53145ba57f6ef4612e0ceed551e2c5e35cc8c
kgogolaa <gogolinex@interia.pl>
2021-04-08 23:26:41 +0200

    Create selekcja
    asfdfasdafsd

---
+++ ConsoleApp1/SelekcjaRuletka.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class SelekcjaRuletka
+    {
+        public Individual Select(List<Individual> individuals)
+        {
+            double[] c;
+            double total;
+            Random random;
+
+            random = new Random();
+            total = 0;
+            c = new double[individuals.Count + 1];
+            c[0] = 0;
+            for (int i = 0; i < individuals.Count; i++)
+            {
+                c[i + 1] = c[i] + individuals[i].FunctionResult;
+                total += individuals[i].FunctionResult;
+            }
+  
+            double r = random.NextDouble() * total;    
+            int a = 0;
+            int b = c.Length - 1;
+            while (b - a > 1)
+            {
+                int mid = (a + b) / 2;
+                if (c[mid] > r) b = mid;
+                else a = mid;
+            }
+
+            return individuals[a];
+        }
+    }
+}
