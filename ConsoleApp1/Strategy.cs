92a3fbf73f5227e15637020536c633bbd5de3016
Tomasz <tomek8b@gmail.com>
2021-04-08 23:26:35 +0200

    dsadsadsa

--- ConsoleApp1/Strategy.cs
+++ ConsoleApp1/Strategy.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleApp1
 {
@@ -8,11 +7,17 @@ namespace ConsoleApp1
     {
         private readonly IndividualGenerator _individualGenerator;
         private readonly SelekcjaNajlepszych _selekcjaNajlepszych;
+        private readonly KrzyzowanieJednopunktowe _krzyzowanieJednopunktowe;
+        private readonly MutacjaJednopunktowa _mutacjaJednopunktowa;
+        private readonly Inwersja _inwersja;
 
-        public Strategy(IndividualGenerator individualGenerator, SelekcjaNajlepszych selekcjaNajlepszych)
+        public Strategy(IndividualGenerator individualGenerator, SelekcjaNajlepszych selekcjaNajlepszych, KrzyzowanieJednopunktowe krzyzowanieJednopunktowe, MutacjaJednopunktowa mutacjaJednopunktowa, Inwersja inwersja)
         {
             _individualGenerator = individualGenerator;
             _selekcjaNajlepszych = selekcjaNajlepszych;
+            _krzyzowanieJednopunktowe = krzyzowanieJednopunktowe;
+            _mutacjaJednopunktowa = mutacjaJednopunktowa;
+            _inwersja = inwersja;
         }
 
         public void Execute(int a, int b, int populationAmount, int numberOfBits, int epochsAmount, int bestAndTournamentChomosomeAmount, int eliteStrategyAmount, double crossProbability, double mutationProbability, double inversionProbability, SelectionMethod selectionMethod, CrossMethod crossMethod, MutationMethod mutationMethod, bool maximization)
@@ -20,13 +25,98 @@ namespace ConsoleApp1
             //Poczatkowa populacja
             var population = _individualGenerator.GenerateList(populationAmount, numberOfBits, a, b);
 
-            //Selekcja
-            var afterSelection = Wyselekcjuj(selectionMethod, population, bestAndTournamentChomosomeAmount);
+            for (int i = 0; i < epochsAmount; i++)
+            {
+                //Selekcja
+                var afterSelection = Wyselekcjuj(selectionMethod, population, bestAndTournamentChomosomeAmount);
 
+                
+                var newPopulation = new List<Individual>();
 
+                //Mutacja, krzyzowanie i inwersja
+                while (newPopulation.Count < population.Count)
+                {
+                    if (ShouldPerformAction(crossProbability) && newPopulation.Count + 1 < population.Count)
+                    {
+                        newPopulation.AddRange(Krzyzuj(crossMethod, afterSelection, a, b));
+                    }
 
+                    if (ShouldPerformAction(mutationProbability) && newPopulation.Count < population.Count)
+                    {
+                        newPopulation.Add(Mutuj(mutationMethod, afterSelection, a, b));
+                    }
 
+                    if (ShouldPerformAction(inversionProbability) && newPopulation.Count < population.Count)
+                    {
+                        newPopulation.Add(Inwersuj(afterSelection, a, b));
+                    }
+                }
 
+            }
+        }
+
+        private Individual Inwersuj(List<Individual> population, int a, int b)
+        {
+            var randomIndividual = GetRandomIndividual(population);
+            return _inwersja.Inwersuj(randomIndividual, a, b);
+        }
+
+        private Individual Mutuj(MutationMethod mutationMethod, List<Individual> population, int a, int b)
+        {
+            var randomIndividual = GetRandomIndividual(population);
+
+            switch (mutationMethod)
+            {
+                case MutationMethod.ONE_POINT:
+                    return _mutacjaJednopunktowa.Mutuj(randomIndividual);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private bool ShouldPerformAction(double probability)
+        {
+            Random random = new Random();
+            var n = random.NextDouble();
+            return n < probability;
+        }
+
+        private Individual GetRandomIndividual(List<Individual> population)
+        {
+            Random random = new Random();
+            var firstIndex = random.Next(0, population.Count);
+
+            return population[firstIndex];
+        }
+
+        private List<Individual> GetTwoRandomIndividuals(List<Individual> population)
+        {
+            Random random = new Random();
+            var firstIndex = random.Next(0, population.Count);
+            int secondIndex = firstIndex;
+            while (firstIndex == secondIndex)
+            {
+                secondIndex = random.Next(0, population.Count);
+            }
+
+            return new List<Individual>
+            {
+                population[firstIndex],
+                population[secondIndex]
+            };
+        }
+
+        private List<Individual> Krzyzuj(CrossMethod crossMethod, List<Individual> population, int a, int b)
+        {
+            var twoRandomIndividuals = GetTwoRandomIndividuals(population);
+
+            switch (crossMethod)
+            {
+                case CrossMethod.ONE_POINT:
+                    return _krzyzowanieJednopunktowe.Krzyzuj(twoRandomIndividuals[0], twoRandomIndividuals[1], a, b);
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
         private List<Individual> Wyselekcjuj(SelectionMethod selectionMethod, List<Individual> population, int bestChromosomeAmount)
@@ -41,20 +131,25 @@ namespace ConsoleApp1
         }
     }
 
-  
-
     public enum SelectionMethod
     {
-        BEST
+        BEST,
+        Kolo_Ruletki,
+        Turniejowa
     }
 
     public enum CrossMethod
     {
-        ONE_POINT
+        ONE_POINT,
+        TWO_POINT,
+        THREE_POINT,
+        JEDNORODNE
     }
 
     public enum MutationMethod
     {
-        ONE_POINT
+        ONE_POINT,
+        TWO_POINT,
+        BRZEGOWA,
     }
 }
